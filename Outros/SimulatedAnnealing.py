4607621b30cb5e5e24ed2b75704a4443e42ac73c
Guilherme Silva Freire <76671441+Guilhos@users.noreply.github.com>
2024-10-19 20:26:37 -0300

    Arrumada no código

--- Outros/SimulatedAnnealing.py
+++ Outros/SimulatedAnnealing.py
@@ -4,11 +4,11 @@ import numpy as np
 rng = np.random.default_rng()
 
 # Função exemplo Aula
-def func(x):
+def func1(x):
     return (x+10)*(x+6)*(x+5)*(x+1)*(x-7)*(x-10)/10000
 
 # Função de Italo
-# def func(x):
+# def func2(x):
 #     if x < -1 or x > 1:
 #         y = 0
 #     else:
@@ -17,7 +17,14 @@ def func(x):
 # fv = np.vectorize(func)
 
 xx = np.linspace(-11,11,500)
-plt.plot(xx, func(xx))
+plt.plot(xx, func1(xx))
+
+dx = 1 # Distância máxima da vizinhança
+t0 = 60 # Temperatura inicial
+alpha = 0.75 # Constante de redução da Temperatura
+x0 = -11 # X inicial
+n1 = 20 # Número de temperaturas
+n2 = 50 # Número de iterações em cada temperatura
 
 def resfr(t, alpha):
     t = alpha*t
@@ -27,46 +34,45 @@ def viz(x,dk,a):
     x = x+dk*a
     return x
 
-dk = 1
-t0 = 60
-alpha = 0.75
-x0 = -11
-n1 = 20
-n2 = 50
-sols = []
-bests = []
+def SA(F, x0, t0, dx, alpha, n1, n2):
+    sols = []
+    bests = []
 
-x = x0
-t = t0
-best = x
+    x = x0
+    t = t0
+    best = x
 
-for i in range(n1):
-    for j in range(n2):
-        a = rng.random() * 2 - 1
-        x1 = viz(x,dk,a)
-        delta = func(x1) - func(x)
-        if x1 > -11 and x1 < 11:
-            if delta < 0:
-                x = x1
-            else:  
-                b = rng.random()
-                temp = b - np.exp(-delta/t)
-                if temp < 0:
+    for i in range(n1):
+        for j in range(n2):
+            a = rng.random() * 2 - 1
+            x1 = viz(x,dx,a)
+            delta = F(x1) - F(x)
+            if x1 > -11 and x1 < 11:
+                if delta < 0:
                     x = x1
-            
-            if func(x) < func(best):
-                best = x
+                else:  
+                    b = rng.random()
+                    temp = b - np.exp(-delta/t)
+                    if temp < 0:
+                        x = x1
+                
+                if F(x) < F(best):
+                    best = x
 
-            sols.append(x)
-            bests.append(best)
-    
-    t = resfr(t,alpha)
+                sols.append(x)
+                bests.append(best)
+        
+        t = resfr(t,alpha)
 
-plt.plot(sols, [func(y) for y in sols], c = 'gray')
-plt.scatter(bests, [func(y) for y in bests], c = 'black')
-plt.scatter([x0, best, sols[-1]],[func(x0),func(best),func(sols[-1])], c = ['b','r','g'])
+    return sols, bests
+
+sol, best = SA(func1, x0, t0, dx, alpha, n1, n2)
+
+def plotter(F, sol, best):
+    plt.plot(sol, [F(y) for y in sol], c = 'gray')
+    plt.scatter(best, [F(y) for y in best], c = 'black')
+    plt.scatter([x0, best[-1], sol[-1]],[F(x0),F(best[-1]),F(sol[-1])], c = ['b','r','g'])
 # Verde é o ultimo ponto visitado
 # Vermelho é o melhor ponto encontrado
 
-
-
+plotter(func1, sol, best)
